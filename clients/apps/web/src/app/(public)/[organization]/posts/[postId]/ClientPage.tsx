655e2ca1395f9c8500e72428095b2bb848409a4a
Emil Widlund <hello@emilwidlund.com>
2023-11-23 16:25:17 +0100

    clients/login: add login button on post pages

--- clients/apps/web/src/app/(public)/[organization]/posts/[postId]/ClientPage.tsx
+++ clients/apps/web/src/app/(public)/[organization]/posts/[postId]/ClientPage.tsx
@@ -1,10 +1,17 @@
+'use client'
+
 import { Post } from '@/components/Feed/data'
+import GithubLoginButton from '@/components/Shared/GithubLoginButton'
 import { ProfileMenu } from '@/components/Shared/ProfileSelection'
+import { useAuth } from '@/hooks'
+import { UserSignupType } from '@polar-sh/sdk'
 import Link from 'next/link'
 import { LogoType } from 'polarkit/components/brand'
 import LongformPost from '../../../../../components/Feed/LongformPost'
 
 export default function Page({ post }: { post: Post }) {
+  const { currentUser } = useAuth()
+
   return (
     <div className="flex w-full flex-col items-center gap-y-16">
       <div className="flex w-full flex-row items-center justify-between px-4">
@@ -12,7 +19,18 @@ export default function Page({ post }: { post: Post }) {
           <LogoType />
         </Link>
         <div>
-          <ProfileMenu />
+          {currentUser ? (
+            <ProfileMenu className="z-50" />
+          ) : (
+            <GithubLoginButton
+              userSignupType={UserSignupType.BACKER}
+              posthogProps={{
+                view: 'Maintainer Page',
+              }}
+              text="Sign in with GitHub"
+              gotoUrl={window.location.href}
+            />
+          )}
         </div>
       </div>
       <LongformPost post={post} />
