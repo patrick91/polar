e5a9019827245cd401976e42887107d568a82081
François Voron <fvoron@gmail.com>
2024-01-04 17:06:19 +0100

    server: fix type issues following FastAPI/Pydantic upgrade

--- server/polar/subscription/endpoints.py
+++ server/polar/subscription/endpoints.py
@@ -5,7 +5,7 @@ from typing import Annotated
 import structlog
 from fastapi import APIRouter, Depends, HTTPException, Query, Response, UploadFile
 from fastapi.responses import StreamingResponse
-from pydantic import UUID4, EmailStr
+from pydantic import UUID4
 
 from polar.auth.dependencies import Auth, UserRequiredAuth
 from polar.authz.service import AccessType, Anonymous, Authz
@@ -378,7 +378,7 @@ async def create_subscribe_session(
     return await subscribe_session_service.create_subscribe_session(
         session,
         subscription_tier,
-        session_create.success_url,
+        str(session_create.success_url),
         auth.subject,
         auth.auth_method,
         authz,
@@ -608,8 +608,7 @@ async def subscriptions_import(
             await subscription_service.create_free_subscription(
                 session,
                 free_subscription_create=FreeSubscriptionCreate(
-                    tier_id=tiers[0].id,
-                    customer_email=EmailStr(email),
+                    tier_id=tiers[0].id, customer_email=email
                 ),
                 auth_subject=Anonymous(),  # do not forward auth!
                 auth_method=None,
